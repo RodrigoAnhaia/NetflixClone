b086319d706e1a22c2019514b67df1c08fa41046
Rodrigo Sanseverino de Anhaia <rodrigoanhaia2@hotmail.com>
2023-01-16 15:04:07 -0300

    Adding TabBar
    Adding to design a TabBar.

---
+++ NetflixClone/NetflixClone/Views/MainTabBarViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainTabBarViewController.swift
+//  NetflixClone
+//
+//  Created by Rodrigo Sanseverino de Anhaia on 16/01/23.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("aqui")
+    }
+}
