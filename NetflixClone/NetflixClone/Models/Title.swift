e72dd657af01a2ff5cae10f4aab425c528d4f30c
Rodrigo Sanseverino de Anhaia <rodrigoanhaia2@hotmail.com>
2023-01-21 20:53:09 -0300

    Refactoring Models
    Refactoring Movie Model to Title Model,
    Deleted Tv Model.

--- NetflixClone/NetflixClone/Models/Title.swift
+++ NetflixClone/NetflixClone/Models/Title.swift
@@ -1,5 +1,5 @@
 //
-//  Tv.swift
+//  Movie.swift
 //  NetflixClone
 //
 //  Created by Rodrigo Sanseverino de Anhaia on 21/01/23.
@@ -7,11 +7,13 @@
 
 import Foundation
 
-struct TrendingTvResponse: Codable {
-    let results: [Tv]
+
+struct TrendingTitleResponse: Codable {
+    let results: [Title]
+    
 }
 
-struct Tv: Codable {
+struct Title: Codable {
     let id: Int
     let media_type: String?
     let original_name: String?
