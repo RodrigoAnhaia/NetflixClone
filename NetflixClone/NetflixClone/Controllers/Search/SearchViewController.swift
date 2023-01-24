9b0e9f74bc2942c17eaa26f81f30088f6e662ae8
Rodrigo Sanseverino de Anhaia <rodrigoanhaia2@hotmail.com>
2023-01-24 16:29:29 -0300

    Created and styled discoverTableView
    Created and styled discoverTableView,
    Created getDiscoverMovies.

--- NetflixClone/NetflixClone/Controllers/Search/SearchViewController.swift
+++ NetflixClone/NetflixClone/Controllers/Search/SearchViewController.swift
@@ -8,22 +8,76 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    
+    private var titles: [Title] = [Title]()
+    
+    private let discoverTable: UITableView = {
+        let table = UITableView()
+        table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .systemBackground
+        self.title = "Search"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        self.setupLayout()
+        self.fetchDiscoverMovies()
+        
+        self.discoverTable.delegate = self
+        self.discoverTable.dataSource = self
+        
+    }
 
-        // Do any additional setup after loading the view.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.discoverTable.frame = self.view.bounds
+    }
+}
+
+extension SearchViewController {
+    fileprivate func setupLayout() {
+        self.view.addSubview(self.discoverTable)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    fileprivate func fetchDiscoverMovies() {
+        APICaller.shared.getDiscoverMovies { [weak self] result in
+            switch result {
+            case .success(let titles):
+                self?.titles = titles
+                DispatchQueue.main.async {
+                    self?.discoverTable.reloadData()
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
-    */
+}
 
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else {
+            return UITableViewCell()
+            
+        }
+        
+        let title = titles[indexPath.row]
+        let model = TitleViewModel(titleName: title.original_title ?? title.original_name ?? "Unknown", posterURL: title.poster_path ?? "")
+        
+        cell.configure(with: model)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
 }
